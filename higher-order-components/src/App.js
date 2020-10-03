d0af02b6a909abc96e1125a0db675ba0d6666924
biagioo <mendolia2328@gmail.com>
2020-10-03 12:03:34 -0400

    imported HoverCounter

--- higher-order-components/src/App.js
+++ higher-order-components/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
       <ClickCounter />
+      <HoverCounter />
     </div>
   );
 }
